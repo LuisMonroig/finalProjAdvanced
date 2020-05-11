940fddc70580d43d8e5894ba02cce6239a0678f9
Ricardo Ramirez Santiago <jricardo0641>
2020-05-11 00:48:07 -0400

    changed the main form to the login form, added a pill class and pill interface

---
+++ PillIdentifier/PillIdentifier/Pill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PillIdentifier
+{
+    class Pill: IPill
+    {
+        private string imprint;
+        private string color;
+        private string shape;
+        private string drugName;
+        private string drugStrength;
+        private DateTime creationTime;
+
+        public Pill(string imprint, string color, string shape, string drugName, string drugStrength, DateTime creationTime)
+        {
+            this.imprint = imprint;
+            this.color = color;
+            this.shape = shape;
+            this.drugName = drugName;
+            this.drugStrength = drugStrength;
+            this.creationTime = creationTime;
+        }
+    }
+}
--- PillIdentifier/PillIdentifier/loginForm.cs
+++ PillIdentifier/PillIdentifier/loginForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PillIdentifier
 {
-    public partial class Form1 : Form
+    public partial class loginForm : Form
     {
-        public Form1()
+        public loginForm()
         {
             InitializeComponent();
         }
