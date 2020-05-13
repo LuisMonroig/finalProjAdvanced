256d5c6ec2f51cb08bc2c7dcffc14151fecd990b
Ricardo Ramirez Santiago <jricardo0641>
2020-05-12 22:44:11 -0400

    added login error message

--- PillIdentifier/PillIdentifier/loginForm.cs
+++ PillIdentifier/PillIdentifier/loginForm.cs
@@ -42,7 +42,7 @@ namespace PillIdentifier
 
             else
             {
-
+                MessageBox.Show("Information incorrect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
