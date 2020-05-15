924560427f3566e97089836af8f99194411522b7
Ricardo Ramirez Santiago <jricardo0641>
2020-05-15 00:48:24 -0400

    connected program to the database

---
+++ PillIdentifier/PillIdentifier/PillDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+/*
+* Filename: PillDB.cs
+* Author: Ricardo J. Ramirez Santiago
+* Description: database for the pill app
+* History:
+*           5/14/2020 - File created. RRS
+*/
+
+namespace PillIdentifier
+{
+    class PillDB
+    {
+        private MySqlConnection connection;
+
+        public PillDB()
+        {
+            //TODO: cambiar el connection setting, ver minuto 24:06 del video de abri 23 del profe
+            string connectionSettings = @"conneciton=stuff, aqui es q van los settings del connection";
+
+            connection = new MySqlConnection(connectionSettings);
+            connection.Open();
+        }
+    }
+}
